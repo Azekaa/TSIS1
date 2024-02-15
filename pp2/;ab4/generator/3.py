33d22358154b8429a57d8114e0ca6aa250c9123b
Azekaa <156984884+Azekaa@users.noreply.github.com>
2024-02-15 11:37:10 +0600

    Update 3.py

--- pp2/;ab4/generator/3.py
+++ pp2/;ab4/generator/3.py
@@ -1,8 +1,22 @@
+# n = int(input())
+# add = []
+# for i in range(n+1):
+#     if(i%12==0):
+#         add.append(i)
+# non = (str(xo) for xo in add)
+# res = ", ".join(non)
+# print(res)
+
+def opa(n):
+    for i in range(n+1):
+      if(i%12==0):
+            yield(i)
 n = int(input())
 add = []
-for i in range(n+1):
-    if(i%12==0):
-        add.append(i)
-non = (str(xo) for xo in add)
+for i in opa(n):
+    add.append(i)
+non = (str(x) for x in add)
 res = ", ".join(non)
 print(res)
+
+
