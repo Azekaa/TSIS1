d2d9ab2bf21dce1c20802b1a5316b7158182612a
Azekaa <156984884+Azekaa@users.noreply.github.com>
2024-02-15 11:37:44 +0600

    Update 5.py

--- pp2/;ab4/generator/5.py
+++ pp2/;ab4/generator/5.py
@@ -1,3 +1,9 @@
+# n = int(input())
+# for i in range(n,-1,-1):
+#     print(i)
+def opa(n):
+    for i in range(n,-1.-1):
+      yield(i)
 n = int(input())
-for i in range(n,-1,-1):
-    print(i)
+for i in opa(n):
+   print(i)
