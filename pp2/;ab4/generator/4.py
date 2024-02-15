b24924f33d95d6373a584f80c9742a6081b7de8b
Azekaa <156984884+Azekaa@users.noreply.github.com>
2024-02-15 11:37:28 +0600

    Update 4.py

--- pp2/;ab4/generator/4.py
+++ pp2/;ab4/generator/4.py
@@ -1,4 +1,12 @@
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#     print(i**2)
+
+def opa(a,b):
+    for i in range(a,b+1):
+      yield(i**2)
 a = int(input())
 b = int(input())
-for i in range(a,b+1):
-    print(i**2)
+for i in opa(a,b):
+   print(i)
